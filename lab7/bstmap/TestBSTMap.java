4a657c78ebb8c5f441b775ac44bd0c07ff4ea927
Hanzhen <zhouhanzhen2023@gmail.com>
2023-12-14 16:58:13 +0800

    Add printInOder() test

--- lab7/bstmap/TestBSTMap.java
+++ lab7/bstmap/TestBSTMap.java
@@ -23,6 +23,9 @@ public class TestBSTMap {
     public void sanityClearTest() {
     	BSTMap<String, Integer> b = new BSTMap<String, Integer>();
         for (int i = 0; i < 455; i++) {
+            if (i == 10) {
+                b.printInOrder();
+            }
             b.put("hi" + i, 1+i);
             //make sure put is working via containsKey and get
             assertTrue( null != b.get("hi" + i) && (b.get("hi"+i).equals(1+i))
