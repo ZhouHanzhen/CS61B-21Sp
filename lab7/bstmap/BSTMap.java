bf9b04592c8902786694874366baa7ce8411d6b2
Hanzhen <zhouhanzhen2023@gmail.com>
2023-12-24 17:18:29 +0800

    Implement the remove() function of BSTMap

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,14 +1,15 @@
 package bstmap;
 
+import edu.princeton.cs.algs4.BST;
 import edu.princeton.cs.algs4.StdOut;
 import org.apache.commons.math3.random.BitsStreamGenerator;
 
 import java.util.*;
 
-public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterable<K> {
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
     private int size;
-    private BSTNode bst;
+    private BSTree bst;
     private List<K> keyList;//按增序记录bst中的key
 
     public BSTMap() {
@@ -17,13 +18,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
         keyList = new ArrayList<>();
     }
 
-    private class BSTNode {
+    private class BSTree {
         K key;
         V val;
-        BSTNode leftChild;
-        BSTNode rightChild;
+        BSTree leftChild;
+        BSTree rightChild;
 
-        BSTNode(K k, V v, BSTNode lchild, BSTNode rChild) {
+        BSTree(K k, V v, BSTree lchild, BSTree rChild) {
             key = k;
             val = v;
             leftChild = lchild;
@@ -51,7 +52,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
             }
         }
 
-        BSTNode BSTGet(K getKey) {
+        BSTree BSTGet(K getKey) {
             int cmp = getKey.compareTo(this.key);
             if (cmp == 0) {
                 return this;
@@ -71,14 +72,14 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
         }
 
         //Insert a node to a BST
-        BSTNode BSTInsert(K insertKey, V insertValue) {
+        BSTree BSTInsert(K insertKey, V insertValue) {
             int cmp = insertKey.compareTo(this.key);
             if (cmp == 0) {
                 this.val = insertValue; //replace the previous value
             }
             else if (cmp < 0) {
                 if (leftChild == null) {
-                    leftChild = new BSTNode(insertKey, insertValue, null, null);
+                    leftChild = new BSTree(insertKey, insertValue, null, null);
                 }
                 else {
                     leftChild = leftChild.BSTInsert(insertKey, insertValue);
@@ -87,7 +88,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
             }
             else {
                 if (rightChild == null) {
-                    rightChild = new BSTNode(insertKey, insertValue, null, null);
+                    rightChild = new BSTree(insertKey, insertValue, null, null);
                 }
                 else {
                     rightChild = rightChild.BSTInsert(insertKey, insertValue);
@@ -95,6 +96,65 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
             }
             return this;
         }
+
+        BSTree BSTDelete(K deleteKey) {
+            int cmp = deleteKey.compareTo(this.key);
+            if (cmp < 0) {
+                if (leftChild != null) {
+                    leftChild = leftChild.BSTDelete(deleteKey);
+                }
+                //如果deleteKey不存在，这棵树不需要变化
+            }
+            else if (cmp > 0) {
+                if (rightChild != null) {
+                    rightChild = rightChild.BSTDelete(deleteKey);
+                }
+                //如果deleteKey不存在，这棵树不需要变化
+            }
+            else {//cmp == 0; this 就是需要被删除的节点
+                if ((rightChild == null) && (leftChild == null)) { //删除节点为叶子节点，
+                    return null;
+                }
+                else if (rightChild == null)  { //删除节点的右孩子为空，只有左孩子
+                    this.key = leftChild.key;
+                    this.val = leftChild.val;
+                    this.rightChild = leftChild.rightChild;
+                    this.leftChild = leftChild.leftChild; //只有左孩子的情况下this的左孩子需要在最后一步修改，才不会影响其他值的改变
+
+                }
+                else if (leftChild == null) { //删除节点的左孩子为空，只有右孩子
+                    this.key = rightChild.key;
+                    this.val = rightChild.val;
+                    this.leftChild = rightChild.leftChild;
+                    this.rightChild = rightChild.rightChild; //只有右孩子的情况下this的右孩子需要在最后一步修改，才不会影响其他值的改变
+                }
+                else { //删除节点左右孩子均不为空，需要找到被删除节点的前驱或者后继节点
+                    BSTree q = this; //q用来指向被删除节点的前驱节点的双亲节点
+                    BSTree s = this.leftChild; //s用来指向被删除节点的前驱节点
+                    while (s.rightChild != null) {
+                        q = s;
+                        s = s.rightChild;
+                    }
+                    //将s的key,value传递给被删除节点this;
+                    this.key = s.key;
+                    this.val = s.val;
+
+                    if (q != this) {
+                        q.rightChild = s.leftChild;
+                    }else {
+                        q.leftChild = s.leftChild;
+                    }
+                }
+            }
+            return this;
+        }
+
+
+
+
+
+
+
 
         void printInOrderBST() {
             if (this.leftChild != null) {
@@ -131,7 +191,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
             return null;
         }
 
-        BSTNode lookup = bst.BSTGet(key);
+        BSTree lookup = bst.BSTGet(key);
         if (lookup == null) {
             return null;
         }
@@ -148,7 +208,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
     public void put(K key, V value) {
         if (bst == null) {
             size += 1;
-            bst = new BSTNode(key, value, null, null);
+            bst = new BSTree(key, value, null, null);
         }
         else {
             if (!containsKey(key)) {
@@ -176,12 +236,26 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V reVal = get(key);
+        if (reVal != null) {
+            bst = bst.BSTDelete(key);
+            size -= 1;
+            keyList.remove(key);
+        }
+        return reVal;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V reVal = get(key);
+        if (reVal == value) {
+            bst = bst.BSTDelete(key);
+            size -= 1;
+            keyList.remove(key);
+            return reVal;
+        }else {
+            return null;
+        }
     }
 
     @Override
@@ -194,9 +268,12 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
         public BSTMapIterator() {
             cur = 0;
         }
+        @Override
         public boolean hasNext() {
             return cur < size;
         }
+
+        @Override
         public K next() {
             K ret = keyList.get(cur);
             cur += 1;
